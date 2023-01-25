d1879cd41c1e64be55dc4f455e5218f9bdc616c7
Alina2801 <117786801+Alina2801@users.noreply.github.com>
2023-01-25 18:20:28 +0600

    Add files via upload
    Signed-off-by: Alina2801 <117786801+Alina2801@users.noreply.github.com>

---
+++ dz1_alina_20-02react/src/components/ui/button.js
@@ -0,0 +1,15 @@
+import classes from './ui.module.css'
+
+const Button = ({ isModal, handleDo, children}) => {
+
+    return (
+        <button
+            onClick={handleDo}
+            className={isModal ? classes.modalButton : classes.button}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button
